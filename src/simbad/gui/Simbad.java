016bb7583da60da8fc2d6ec2b6b06377b3aee513
Alan Grenet <79207864+Kookee214@users.noreply.github.com>
2022-01-10 09:35:32 +0100

    simulator en public

--- src/simbad/gui/Simbad.java
+++ src/simbad/gui/Simbad.java
@@ -66,7 +66,7 @@ public class Simbad extends JFrame implements ActionListener {
     ControlWindow controlWindow=null;
     GameplayWindow gameplayWindow = null;
     World world;
-    Simulator simulator;
+    public Simulator simulator;
     Console console=null;
     AgentInspector agentInspector=null;
     boolean backgroundMode;
@@ -86,6 +86,7 @@ public class Simbad extends JFrame implements ActionListener {
         createGUI();
         start(ed);
         setVisible(true);
+        ed.setSimbad(this);
        
           }
  
